9264deeadffe20752dbe5a631ea472da6554938e
[ckorski] <[ckorski.fst@gmail.com]>
2015-06-12 16:31:15 -0700

    modified files

--- js/app.js
+++ js/app.js
@@ -1,24 +1,18 @@
-$(document).ready(function(){
+/*$(document).ready(function(){
 
-function addText(){
-    var input = document.getElementById('items').value;
-    var node=document.createElement("li").append;
-    var textnode=document.createTextNode(input);
-    node.appendChild(textnode);
-    document.getElementById('addTask').appendChild(node);
-    
-    var removeTask = document.createElement('input');
-    removeTask.setAttribute('type', 'button');
-    removeTask.setAttribute("value", "Remove");
-    removeTask.setAttribute("id", "removeButton");
-    removeTask.addEventListener('click', function(e) {
-        node.parentNode.removeChild(node);
-    }, false);
-    node.appendChild(removeTask);
-    
-    
-}
-
+ $('#btn-add').click(function() {
+        var item = $('#input-items')
+        $('ul').prepend("<li>"+item.val()+"</li>");
+    });
+    $('ul').on('click', 'li', function() {
+        $(this).remove();
+        });
+});
+--------------------------------------------
+ $('ul').on('click', 'li', function(event){
+  // fires when any LIs are clicked on
+  // including LIs that aren't on the page when it is initially loaded
+});
 
 //cross off list items
     $('#list').on('click', 'li', function(){$(this).toggleClass('strike'); $(this).children('.box').toggleClass('Checked');});
@@ -32,3 +26,26 @@ function addText(){
 
 });
 
+$("#header ul").append('<li><a href="/user/messages"><span class="tab">Message Center</span></a></li>');
+
+*/
+
+$(document).ready(function(){  
+    // 1st get text from input box and display it on page
+    $("#btn-add").on("click", function(){
+        event.preventDefault(); //with this when you can also hit "enter" instead of clicking on the button all the time
+        var item = $("#input-items").val();
+    // If any the length of any item types is less or equal to zero you will get an alert message
+    if (item <= 0) {
+        alert ("You didn't add anything")
+    // Otherwise, if you type in your list - it will appear!
+    } else {
+        $(".ul-theList").append("<li>" +"<input type='checkbox' id='myCheck'>" + "" + item + "<img class='delete' src='img/trsh.png'/>" + "" + "</li>");
+        $("#input-items").val("");
+    }
+    })  
+    // delete item
+    $("ul").on('click', '.delete', function() {$(this).closest("li").remove();});
+    // check off an item and put a strikethrough
+    $("ul").on('click', '#myCheck', function() {$(this).closest("li").appendTo(".completed-list");});
+}); 
